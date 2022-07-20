4248c620d2c503a6245a2fdce94f6dbab9c2a7d3
Pablo Agustin Ndure <ndurepablo@gmail.com>
2022-07-20 19:12:59 -0300

    views.py updated - register_form

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from mainapp.forms import RegisterForm
 
 # Create your views here.
 def index(request):
@@ -8,4 +10,24 @@ def index(request):
 def about(request):
     return render(request, 'mainapp/about.html', {
         "title": "About",
+    })
+    
+def register_page(request):
+    register_form = RegisterForm()
+    
+    # Recibe el método en mediante el form
+    if request.method == "POST":
+        # envia la rq mediante POST
+        register_form = RegisterForm(request.POST)
+        # si el formulario es válido
+        if register_form.is_valid():
+            # guarda el registro
+            register_form.save()
+            # redirecciona a la página de inicio
+            return redirect('index')
+            
+    
+    return render(request, "users/register.html", {
+        "title": "Registro",
+        "register_form": register_form,
     })
